f1e9eebd3968d0a56a8109ac8d71751c0c96a323
Hongbo Wu <hongbo@omnivore.app>
2023-01-31 12:14:23 +0800

    fix: add back the ribbon icon for the ease of usage

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon";
 import Mustache from "mustache";
 import {
+  addIcon,
   App,
   normalizePath,
   Notice,
@@ -117,6 +118,23 @@ export default class OmnivorePlugin extends Plugin {
         new Notice("Omnivore Last Sync reset");
         this.fetchOmnivore();
       },
+    });
+
+    const iconId = "Omnivore";
+    // add icon
+    addIcon(
+      iconId,
+      `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 21 21" version="1.1">
+    <g id="surface1">
+    <path style=" stroke:none;fill-rule:evenodd;fill:rgb(63.137255%,62.352941%,61.176471%);fill-opacity:1;" d="M 9.839844 0.0234375 C 15.9375 -0.382812 21.058594 4.171875 21.140625 10.269531 C 21.140625 10.921875 20.976562 11.734375 20.816406 12.464844 C 20.410156 14.253906 18.785156 15.472656 16.992188 15.472656 L 16.914062 15.472656 C 14.71875 15.472656 13.253906 13.601562 13.253906 11.566406 L 13.253906 9.292969 L 11.953125 11.242188 L 11.871094 11.324219 C 11.140625 11.972656 10.082031 11.972656 9.351562 11.324219 L 9.1875 11.242188 L 7.808594 9.210938 L 7.808594 14.496094 L 5.9375 14.496094 L 5.9375 8.15625 C 5.9375 6.855469 7.484375 6.042969 8.539062 7.015625 L 8.621094 7.097656 L 10.488281 9.859375 L 12.441406 7.179688 L 12.519531 7.097656 C 13.496094 6.285156 15.121094 6.933594 15.121094 8.316406 L 15.121094 11.570312 C 15.121094 12.789062 15.851562 13.601562 16.910156 13.601562 L 16.992188 13.601562 C 17.964844 13.601562 18.777344 12.953125 19.023438 12.058594 C 19.183594 11.328125 19.265625 10.757812 19.265625 10.269531 C 19.265625 5.308594 15.039062 1.570312 10 1.898438 C 5.6875 2.140625 2.195312 5.636719 1.871094 9.863281 C 1.542969 14.90625 5.53125 19.132812 10.488281 19.132812 L 10.488281 21 C 4.390625 21 -0.410156 15.878906 -0.00390625 9.78125 C 0.40625 4.578125 4.554688 0.351562 9.839844 0.0234375 Z M 9.839844 0.0234375 "/>
+    </g>
+    </svg>`
+    );
+
+    // This creates an icon in the left ribbon.
+    this.addRibbonIcon(iconId, iconId, async (evt: MouseEvent) => {
+      // Called when the user clicks the icon.
+      await this.fetchOmnivore();
     });
 
     // This adds a settings tab so the user can configure various aspects of the plugin
